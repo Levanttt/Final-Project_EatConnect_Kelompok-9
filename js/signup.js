ac6a239f80cdaa97bc0453854f63c23f49858fa8
Levanttt <luthfiardi66@gmail.com>
2025-06-24 09:57:51 +0700

    Finishing-Login-Flow

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,25 @@
-document
-  .getElementById("registerForm")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+  const registerForm = document.getElementById('registerForm');
+  const popup = document.getElementById('popup-berhasil');
+  if (registerForm) {
+    registerForm.addEventListener('submit', function(e) {
+      e.preventDefault();
 
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
 
-    if (password !== confirmPassword) {
-      alert("Kata sandi tidak cocok!");
-      return;
-    }
+      if (password !== confirmPassword) {
+        alert("Kata sandi tidak cocok!");
+        return;
+      }
 
-    alert("Registrasi berhasil!");
-    this.reset();
-  });
+      // Tampilkan popup
+      popup.classList.add('active');
+
+      // Redirect ke login.html setelah 2 detik
+      setTimeout(function() {
+        window.location.href = 'login.html';
+      }, 2000);
+    });
+  }
+});
